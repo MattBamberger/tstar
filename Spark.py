e3c450de5f05584a2d01bda7b3961a0c5d05e421
MattBamberger <mattb@intelligentartifice.com>
2016-08-23 06:58:50 +0000

    Starting to add real patterns

--- Spark.py
+++ Spark.py
@@ -1,28 +1,60 @@
+import math
+import random
+from ColorCycler import ColorCycler
+
+
 ##
 ## Spark
 ## A shiny object that races around the screen.
 ##
 class Spark(object):
-	# init
-	def __init__(self, strip):
-		self.x = 8
-		self.y = 8
-		self.colorCycler = ColorCycler(self.strip.frameRate, [(255.0, 0.0, 0.0), (0.0, 0.0, 255.0)])
-		self.radius = 2.0
-
-		self.direction = 0.0
-		self.speed = 1.0 / self.strip.frameRate
-		return
+    # init
+    def __init__(self, strip):
+        self.strip = strip
+        self.active = False
+        return
 
 
-	def run(self, strip):
-		self.colorCycler.cycle()
+    def run(self):
+        if not self.active:
+            self.maybeActivate()
+            return
+				
+        self.colorCycler.cycle()
 
-		dx = math.sin(self.direction) * self.speed
-		dy = math.cos(self.direction) * self.speed
+        dx = math.cos(self.direction) * self.speed
+        dy = math.sin(self.direction) * self.speed
 
-		self.x += dx
-		self.y += dy
+        self.x += dx
+        self.y += dy
 
-		strip.drawBall(self.x, self.y, self.radius, self.colorCycler.currentColor)
-		return
+        if (self.x < 0 and dx < 0) or (self.x > 15 + self.radius and dx > 0) or (self.y < 0 and dy < 0) or (self.y > 15 + self.radius and dy > 0):
+            self.active = False
+
+        if self.radius == 0.0:
+            x = int(self.x)
+            y = int(self.y)
+            if x >= 0 and x <= 15 and y >= 0 and y <= 15:
+                self.strip.augmentGridPixel(x, y, self.colorCycler.currentColor)
+        else:
+            self.strip.drawBall(self.x, self.y, self.radius, self.colorCycler.currentColor)
+        return
+
+
+    def maybeActivate(self):
+        if random.random() < self.activateChance / self.strip.frameRate:
+            self.activate()
+        return
+
+
+    def activate(self):
+        self.active = True
+        
+        self.x = random.random() * 16
+        self.y = random.random() * 16
+        self.colorCycler = ColorCycler(1 / self.strip.frameRate, [(255.0, 0.0, 0.0), (0.0, 0.0, 255.0)])
+        self.radius = 2.0
+
+        self.direction = random.random() * 2 * math.pi
+        self.speed = 5.0 / self.strip.frameRate
+        return
--- TPack.py
+++ TPack.py
@@ -1,7 +1,10 @@
+import time
 from TStrip import TStrip
 from ColorCycler import ColorCycler
 from Spark import Spark
-import time
+from MatrixRain import MatrixRain
+from AcidRain import AcidRain
+from Slasher import Slasher
 
 
 
@@ -12,8 +15,8 @@ import time
 class TPack(object):
   
     # init  
-    # Animation 1: violet background with orbiting spark
-    # Animation 2: Matrix
+    # Animation 1: Matrix
+    # Animation 2: 
     def __init__(self, dimmer, animation):
         # Init the strip as a matrix
         self.strip = TStrip(256, dimmer)
@@ -30,42 +33,27 @@ class TPack(object):
         return
 
 
-    # init1
-    # Inits the cycling spark animation
     def init1(self):
-        self.sparkSpeed = 60        # Spark speed in pixels per second
-
-        self.strip.paint((0x10, 0, 0x10))
-        self.sparkPos = 0.0
-        self.xSpark = 0
-        self.ySpark = 0
+        self.sparks = []
+        for i in range(16):
+            self.sparks.append(MatrixRain(self.strip))
+        self.fadeRate = .80
         return
 
 
-    # init2
-    # Inits the speedball animation
     def init2(self):
-        self.ballX = 0
-        self.ballY = 0
-        self.ballDX = 1.2
-        self.ballDY = 0.73
-
-        self.colorCycler = ColorCycler(.01, [
-            (255.0,     0.0,        255.0),
-            (255.0,     0.0,        0.0),
-            (0.0,       255.0,      0.0),
-            ])
-
-        self.strip.paint((0, 0, 0))
+        self.sparks = []
+        for i in range(30):
+            self.sparks.append(AcidRain(self.strip))
+        self.fadeRate = .80
         return
 
 
-    # init3
-    # Inits a spark playground
     def init3(self):
-        self.spark = Spark(self.strip)
-
-        self.strip.paint((0, 0, 0))
+        self.sparks = []
+        for i in range(2):
+            self.sparks.append(Slasher(self.strip))
+        self.fadeRate = .80
         return
 
 
@@ -75,75 +63,20 @@ class TPack(object):
         while True:
             self.strip.startFrame()
 
-            if self.animation == 1:
-                self.run1()
-            elif self.animation == 2:
-                self.run2()
-            elif self.animation == 3:
-                self.run3()
+            self.strip.fade(self.fadeRate)
+
+            for spark in self.sparks:
+                spark.run()
 
             self.strip.endFrame()
-
-
-    # run1
-    # Runs the orbiting spark animation
-    def run1(self):
-        # Move the spark
-        self.strip.setGridPixel(self.xSpark, self.ySpark,(0x10, 0, 0x10))
-        self.sparkPos += (self.sparkSpeed / self.strip.frameRate)
-        if self.sparkPos >= 60:
-            self.sparkPos = 0
-
-        if self.sparkPos <= 15:
-            self.xSpark = int(self.sparkPos)
-            self.ySpark = 0
-        elif self.sparkPos <= 30:
-            self.xSpark = 15
-            self.ySpark = int(self.sparkPos - 15)
-        elif self.sparkPos <= 45:
-            self.xSpark = int(15 - (self.sparkPos - 30))
-            self.ySpark = 15
-        else:
-            self.xSpark = 0
-            self.ySpark = int(15 - (self.sparkPos - 45))
-
-        self.strip.setGridPixel(self.xSpark, self.ySpark, (0xff, 0xff, 0xff))
-        return
-
-
-    # run2
-    # Runs the speedball animation
-    def run2(self):
-        self.strip.fade(.80)
-        self.colorCycler.cycle()
-        
-        self.ballX += self.ballDX
-        if self.ballX < 0:
-            self.ballX = - self.ballX
-            self.ballDX = - self.ballDX
-        if self.ballX > 15:
-            self.ballX = 15 - (self.ballX - 15)
-            self.ballDX = - self.ballDX
-
-        self.ballY += self.ballDY
-        if self.ballY < 0:
-            self.ballY = - self.ballY
-            self.ballDY = - self.ballDY
-        if self.ballY > 15:
-            self.ballY = 15 - (self.ballY - 15)
-            self.ballDY = - self.ballDY
-
-        self.strip.drawBall(self.ballX, self.ballY, 1.5, self.colorCycler.currentColor)
-        return
 
 
     # run3
     # Runs the spark playground
     def run3(self):
-        self.spark.run(self.strip)
         return
 
 
 
-pack = TPack(0, 2)
+pack = TPack(0, 3)
 pack.run()
